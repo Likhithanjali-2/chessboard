818b71ad16b2f6915d83adcc05d6e1fc3378ff33
Likhithanjali-2 <liki2444@gmail.com>
2020-03-09 15:35:29 +0530

    modified x->y & y->x cordinates ,input form

--- chessboard.js
+++ chessboard.js
@@ -1,68 +1,68 @@
-function turnTheQueen(dir, queen) {
+function changeTheDirection(dir, queen) {
   queen["direction"] = dir;
   return;
 }
 
-function moveForward(pos, boardLength, step = 1) {
-  switch (queen["direction"]) {
+function moveTowardsDirection(direction, pos, step, boardLength) {
+  switch (direction) {
     case "N":
-      if (pos["y_cordinate"] - step >= 0) {
-        pos["y_cordinate"] -= step;
-      } else console.log("Queen reached to the end of the board");
-      break;
-
-    case "E":
-      if (pos["x_cordinate"] + step < boardLength) {
-        pos["x_cordinate"] += step;
-      } else console.log("Queen reached to the end of the board");
-      break;
-
-    case "S":
-      if (pos["y_cordinate"] + step < boardLength) {
-        pos["y_cordinate"] += step;
-      } else console.log("Queen reached to the end of the board");
-      break;
-
-    case "W":
       if (pos["x_cordinate"] - step >= 0) {
         pos["x_cordinate"] -= step;
       } else console.log("Queen reached to the end of the board");
       break;
 
-    case "NE":
-      if (
-        pos["x_cordinate"] + step < boardLength &&
-        pos["y_cordinate"] - step >= 0
-      ) {
+    case "E":
+      if (pos["y_cordinate"] + step < boardLength) {
+        pos["y_cordinate"] += step;
+      } else console.log("Queen reached to the end of the board");
+      break;
+
+    case "S":
+      if (pos["x_cordinate"] + step < boardLength) {
         pos["x_cordinate"] += step;
+      } else console.log("Queen reached to the end of the board");
+      break;
+
+    case "W":
+      if (pos["y_cordinate"] - step >= 0) {
         pos["y_cordinate"] -= step;
       } else console.log("Queen reached to the end of the board");
       break;
 
-    case "NW":
-      if (pos["x_cordinate"] - step >= 0 && pos["y_cordinate"] - step >= 0) {
+    case "NE":
+      if (
+        pos["y_cordinate"] + step < boardLength &&
+        pos["x_cordinate"] - step >= 0
+      ) {
+        pos["y_cordinate"] += step;
         pos["x_cordinate"] -= step;
+      } else console.log("Queen reached to the end of the board");
+      break;
+
+    case "NW":
+      if (pos["y_cordinate"] - step >= 0 && pos["x_cordinate"] - step >= 0) {
         pos["y_cordinate"] -= step;
+        pos["x_cordinate"] -= step;
       } else console.log("Queen reached to the end of the board");
       break;
 
     case "SE":
       if (
-        pos["x_cordinate"] + step < boardLength &&
-        pos["y_cordinate"] + step < boardLength
+        pos["y_cordinate"] + step < boardLength &&
+        pos["x_cordinate"] + step < boardLength
       ) {
-        pos["x_cordinate"] += step;
         pos["y_cordinate"] += step;
+        pos["x_cordinate"] += step;
       } else console.log("Queen reached to the end of the board");
       break;
 
     case "SW":
       if (
-        pos["x_cordinate"] - step >= 0 &&
-        pos["y_cordinate"] + step < boardLength
+        pos["y_cordinate"] - step >= 0 &&
+        pos["x_cordinate"] + step < boardLength
       ) {
-        pos["x_cordinate"] -= step;
-        pos["y_cordinate"] += step;
+        pos["y_cordinate"] -= step;
+        pos["x_cordinate"] += step;
       } else console.log("Queen reached to the end of the board");
       break;
 
@@ -78,67 +78,61 @@ function isNewPosition(arr, element) {
   return true;
 }
 
-function trackThePosAndUpdate(queen, board) {
+function trackThePosAndUpdate(queenPos, whereAbouts, board) {
   queenStatus =
-    board[queen["current_position"]["x_cordinate"]][
-      queen["current_position"]["y_cordinate"]
+    board[queenPos["x_cordinate"]][
+      queenPos["y_cordinate"]
     ];
-  if (isNewPosition(queen["whereAbouts"], queenStatus))
-    queen["whereAbouts"].push(queenStatus);
+  if (isNewPosition(whereAbouts, queenStatus))
+    whereAbouts.push(queenStatus);
 }
 
-var board = [
-  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
-  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
-  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
-  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
-  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
-  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
-  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
-  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]
-];
+function init() {
+  var board = [
+    ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+    ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+    ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+    ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+    ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+    ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+    ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+    ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]
+  ];
+  var queen = {
+    current_position: {
+      x_cordinate: 2,
+      y_cordinate: 2
+    },
+    direction: "s",
+    whereAbouts: ["c6"]
+  };
+  return board, queen;
+}
 
-var queen = {
-  current_position: {
-    x_cordinate: 2,
-    y_cordinate: 2
-  },
-  direction: "s",
-  whereAbouts: ["c6"]
-};
+function takeInputFromUser() {
+  var userInput = [];
+  do {
+    input = prompt("Enter input direction and steps to move the queen :)");
+    userInput.push(input);
+  } while (input != "");
+  return userInput;
+}
 
 function main() {
-  var boardLen = board.length;
-  var pos = queen["current_position"];
-  var nextTurn = "y";
+  var ui, direction, noOfSteps;
+  var userInput = takeInputFromUser() //["E1", "N2", "S3", "W2", "NE2", "SE2", "NW2", "SW2"];
+  board, queen = init();
 
-  do {
-    //turn the queen to the given direction
-    var dir = prompt("Enter the direction of Queen to move ");
-    turnTheQueen(dir, queen);
+  for (var i = 0; i < userInput.length - 1; i++) {
+    ui = userInput[i];
+    direction = ui.slice(0, ui.length - 1);
+    noOfSteps = Number(ui.slice(ui.length - 1));
 
-    //move the queen
-    var movingChoice = prompt("Want to move or jump ? ").toLowerCase();
-    if (movingChoice == "move") {
-      console.log("moving forward one step");
-      moveForward(pos, boardLen);
-    } else if (movingChoice == "jump") {
-      console.log("jumping!!");
-      moveForward(
-        pos,
-        boardLen,
-        Number(prompt("Enter how many steps you want to jump ? "))
-      );
-    } else {
-      console.log("Please enter data properly");
-    }
-
-    //tracking the queen position and update
-    trackThePosAndUpdate(queen, board);
-    console.log(queen);
-
-    nextTurn = prompt("Do you want to continue queen movement if yes press Y");
-  } while (nextTurn == "y" || nextTurn == "Y");
+    changeTheDirection(direction, queen);
+    moveTowardsDirection(queen["direction"], queen['current_position'], noOfSteps, board.length);
+    trackThePosAndUpdate(queen["current_position"], queen['whereAbouts'], board);
+  }
+  console.log(queen);
+  return;
 }
-
 main();
